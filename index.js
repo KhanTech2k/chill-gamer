70911d485450168005b1cbe1e33896a8d332d443
PIASH <zmnobab1999@gmail.com>
2024-12-05 19:38:07 +0600

    read operation done for watchlist collection using specific id

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/watchlist/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await watchlistCollection.findOne(query)
+            res.send(result)
+        })
         app.put('/review/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
